9cd483ce705bd6f180cb1ef6adca0dcdfc552534
Murat Çakmak <muratcakmak5252@gmail.com>
2022-01-02 23:21:44 +0300

    Auth Sanctum added

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,14 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group(['prefix' => 'auth'], function(){
+    Route::post("register", [AuthController::class, 'register'])->name('auth.register');
+    Route::post("login", [AuthController::class, 'login'])->name('auth.login');
+});
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'admin', 'middleware' => 'auth:sanctum'], function(){
+
 });
